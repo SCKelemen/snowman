5b0475d25f7c05ddbe547406d420b0391bcde4bf
Samuel Kelemen <Samuel@Kelemen.us>
2018-05-07 12:01:03 -0400

    indexes

--- src/components/snowflake.tsx
+++ src/components/snowflake.tsx
@@ -5,13 +5,13 @@ class Snowflake extends React.Component {
   public render() {
     return (
       <div className="Snowflake">
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123456789 </span>
-        <span className="Segment"> 0123 </span>
+        <span className="Segment badge" data-badge="64"> 0123456789 </span>
+        <span className="Segment badge" data-badge="54"> 0123456789 </span>
+        <span className="Segment badge" data-badge="44"> 0123456789 </span>
+        <span className="Segment badge" data-badge="34"> 0123456789 </span>
+        <span className="Segment badge" data-badge="24"> 0123456789 </span>
+        <span className="Segment badge" data-badge="14"> 0123456789 </span>
+        <span className="Segment badge" data-badge="4 "> 0123 </span>
       </div>
     );
   }
