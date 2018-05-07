6eb4f4bceb2f8778af49c1ad0324cde649ddfcfe
Samuel Kelemen <Samuel@Kelemen.us>
2018-05-07 14:05:04 -0400

    moved attributes to state, as the component needs to manage it, and mutate it over time

--- src/components/attribute.tsx
+++ src/components/attribute.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
 import './snowflake.css';
 
-interface IAttributeProps {
- // Number: number | null;
+
+
+interface IAttributeState {
   Name:   string | null;
   Start:  number | null;
   End:    number | null;
   Length: number | null;
-  Offset: string | null;
+  Offset: string | null
 }
 
-
-class Attribute extends React.Component<IAttributeProps, any> {
-  constructor(props: IAttributeProps) {
+class Attribute extends React.Component<any, IAttributeState> {
+  constructor(props: any) {
     super(props);
-}
-
+    this.state = { Name: "TimeStamp", Start: 64, End: 54, Length: 10, Offset: "0x00FF" }
+  }
+  
   public render() {
     return (
-      <div className="Attribute">
-      
-        <span className="itemName">       {this.props.Name}   </span>
-        <span className="startPosition">  {this.props.Start}  </span>
-        <span className="endPosition">    {this.props.End}    </span>
-        <span className="length">         {this.props.Length} </span>
-        <span className="offset">         {this.props.Offset} </span>
+      <div className="Attribute">      
+        <span className="itemName">       {this.state.Name}   </span>
+        <span className="startPosition">  {this.state.Start}  </span>
+        <span className="endPosition">    {this.state.End}    </span>
+        <span className="length">         {this.state.Length} </span>
+        <span className="offset">         {this.state.Offset} </span>
       </div>
     );
   }
